bf9a44e8e6a85ec7a97ec2f2129e6c4b755f86a8
01Keven <kevenchristiancode@gmail.com>
2025-10-02 19:56:00 -0300

    feat: useState

--- curso-base-react/src/App.tsx
+++ curso-base-react/src/App.tsx
@@ -1,27 +1,28 @@
-interface ICardProps {
-  title: string
-}
+// interface ICardProps {
+//   title: string
 
-export const Card = (props: ICardProps) => {
-  return (
-    <div>
-      <span>Title: {props.title} </span>
-      <div>Content</div>
-      <div>Footer</div>
-    </div>
-  )
-}
+// }
+
+// export const Card = (props: ICardProps) => {
+//   return (
+//     <div>
+//       <span>Title: {props.title} </span>
+//       <div>Content</div>
+//       <div>Footer</div>
+//     </div>
+//   )
+// }
+
+import { useState } from "react";
 
 
 export function App() {
 
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      ola mundo
-      <Card title="Title 1"/>
-      <Card title="Title 1"/>
-      <Card title="Title 3"/>
-      <Card title="Title 1"/>
+      <button onClick={() => setCount(count + 1)}>Click {count}</button>
     </>
   )
 }
