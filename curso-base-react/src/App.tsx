9355bf1b48e0478ded901bbeec656700067c7bb9
01Keven <kevenchristiancode@gmail.com>
2025-10-02 19:30:35 -0300

    feat: update README and implement Card component in App

--- curso-base-react/src/App.tsx
+++ curso-base-react/src/App.tsx
@@ -1,9 +1,27 @@
+interface ICardProps {
+  title: string
+}
+
+export const Card = (props: ICardProps) => {
+  return (
+    <div>
+      <span>Title: {props.title} </span>
+      <div>Content</div>
+      <div>Footer</div>
+    </div>
+  )
+}
+
 
 export function App() {
 
   return (
     <>
       ola mundo
+      <Card title="Title 1"/>
+      <Card title="Title 1"/>
+      <Card title="Title 3"/>
+      <Card title="Title 1"/>
     </>
   )
 }
--- curso-base-react/src/main.tsx
+++ curso-base-react/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // named import
-import { App } from './App'
+import { App} from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/* Componente react */}
     <App />
   </StrictMode>,
 )
