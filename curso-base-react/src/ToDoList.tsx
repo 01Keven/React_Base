d55b28cea58ff4bcbdeb8d318cf9fdd94532a8e1
01Keven <kevenchristiancode@gmail.com>
2025-10-03 17:33:59 -0300

    feat: enhance ToDoList to include completion status for tasks

--- curso-base-react/src/ToDoList.tsx
+++ curso-base-react/src/ToDoList.tsx
@@ -6,9 +6,9 @@ export function ToDoList() {
     const [value, setValue] = useState('');
 
     const [list, setList] = useState([
-        {id: '1', label: 'Bater o Ponto'},
-        {id: '2', label: 'Esperar horario de pico'},
-        {id: '3', label: 'Ir ate os setores'},
+        {id: '1', label: 'Bater o Ponto', complete: false},
+        {id: '2', label: 'Esperar horario de pico', complete: false},
+        {id: '3', label: 'Ir ate os setores', complete: false},
     ]);
 
     console.log(list[0]);
@@ -21,16 +21,34 @@ export function ToDoList() {
 
             <button onClick={() => {
                 setList([ 
-                    ...list, {id: (list.length + 1).toString(), label: value}]);
+                    ...list, {id: (list.length + 1).toString(), label: value, complete: false}]);
                     setValue('');
-            }}
-                
-                >Add</button>
-
+            }} >Add</button>
 
             <ol>
                 {list.map(listItem => (
-                    <li key={listItem.id}> {listItem.label} </li>
+                    <li 
+                    
+                    key={listItem.id}
+                    style={{textDecoration: listItem.complete ? 'line-through' : 'none'}}
+
+                    > 
+                    {listItem.label} 
+
+                    <button onClick={() => 
+                        setList([...list.filter(item => item.id !== listItem.id)])}> 🗑 
+                    </button>
+
+                    <button onClick={() => 
+                        setList(list.map(item =>
+                            item.id === listItem.id
+                            ? {...item, complete: !item.complete}
+                            : item
+                        ))
+                    }> Check </button>
+                    
+                    
+                    </li>
                 ))}
             </ol>
         </div>
